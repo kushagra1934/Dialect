78d836466837d9ec8be91ede2aac8caac4709afe
Kushagra Saxena <kushagra.2022ug1093@iiitranchi.ac.in>
2025-07-08 00:51:19 +0530

    added chat route and controller using stream token

--- backend/src/server.js
+++ backend/src/server.js
@@ -5,7 +5,9 @@ import cookieParser from "cookie-parser"
 dotenv.config();
 
 import authRoutes from "./routes/auth.route.js";
-import userRoutes from "./routes/user.route.js"
+import userRoutes from "./routes/user.route.js";
+import chatRoutes from "./routes/chat.route.js"
+
 import { connectDB } from "./lib/db.js";
 
 const app = express();
@@ -16,6 +18,8 @@ app.use(cookieParser())
 
 app.use("/api/auth", authRoutes);
 app.use("/api/users", userRoutes);
+app.use("/api/chat", chatRoutes);
+
 
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
