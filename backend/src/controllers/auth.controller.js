342b8bd5cbb76b92f7bb930b3136a5efbd0c2ac1
Kushagra Saxena <kushagra.2022ug1093@iiitranchi.ac.in>
2025-07-08 00:12:41 +0530

    added auth functionality, auth middleware (protected routes), user routes, user controller

--- backend/src/controllers/auth.controller.js
+++ backend/src/controllers/auth.controller.js
@@ -1,3 +1,4 @@
+import { upsertStreamUser } from "../lib/stream.js";
 import User from "../models/User.js";
 import jwt from "jsonwebtoken";
 
@@ -42,8 +43,17 @@ export async function signup(req, res) {
       profilePic: randomAvatar,
     });
 
-
     //TODO: create the user in stream as well
+    try {
+      await upsertStreamUser({
+        id: newUser._id.toString(),
+        name: newUser.fullName,
+        image: newUser.profilePic || "",
+      });
+      console.log(`Stream user created for ${newUser.fullName}`);
+    } catch (error) {
+      console.log("Error creating Stream User", error);
+    }
 
     const token = jwt.sign(
       { userId: newUser._id },
@@ -64,19 +74,17 @@ export async function signup(req, res) {
       success: true,
       user: newUser,
     });
-
-
   } catch (error) {
-    console.log("Error in Signup Controller", error)
+    console.log("Error in Signup Controller", error);
     res.status(500).json({
-        messsage:"Internal Server Error"
-    })
+      messsage: "Internal Server Error",
+    });
   }
-//   res.send("Signup Route");
+  //   res.send("Signup Route");
 }
 
 export async function login(req, res) {
-  const {email,password}=req.body;
+  const { email, password } = req.body;
 
   try {
     if (!email || !password) {
@@ -100,13 +108,9 @@ export async function login(req, res) {
       });
     }
 
-    const token = jwt.sign(
-      { userId: user._id },
-      process.env.JWT_SECRET_KEY,
-      {
-        expiresIn: "7d",
-      }
-    );
+    const token = jwt.sign({ userId: user._id }, process.env.JWT_SECRET_KEY, {
+      expiresIn: "7d",
+    });
 
     res.cookie("jwt", token, {
       maxAge: 7 * 24 * 60 * 60 * 1000,
@@ -121,17 +125,83 @@ export async function login(req, res) {
     });
   } catch (error) {
     res.status(500).json({
-        message: "Internal Server Error"
-    })
+      message: "Internal Server Error",
+    });
   }
-
 }
 
-
 export function logout(req, res) {
-    res.clearCookie("jwt");
+  res.clearCookie("jwt");
+  res.status(200).json({
+    success: true,
+    message: "Logout successful",
+  });
+}
+
+export async function onboard(req, res) {
+  try {
+    const userId = req.user._id;
+
+    const { fullName, bio, nativeLanguage, learningLanguage, location } =
+      req.body;
+
+    if (
+      !fullName ||
+      !bio ||
+      !nativeLanguage ||
+      !learningLanguage ||
+      !location
+    ) {
+      return res.status(400).json({
+        message: "All fields are required",
+        missingFields: [
+          !fullName && "fullName",
+          !bio && "bio",
+          !nativeLanguage && "nativeLanguage",
+          !learningLanguage && "learningLanguage",
+          !location && "location",
+        ].filter(Boolean),
+      });
+    }
+
+    const updatedUser = await User.findByIdAndUpdate(
+      userId,
+      {
+        ...req.body,
+        isOnboarded: true,
+      },
+      { new: true }
+    );
+
+    if (!updatedUser) {
+      return res.status(404).json({
+        message: "User not found",
+      });
+    }
+
+    //TODO : Update user info in stream
+
+    try {
+        await upsertStreamUser({
+          id: updatedUser._id.toString(),
+          name:updatedUser.fullName,
+          image: updatedUser.profilePic || ""
+        });
+        console.log(`Stream user updated for ${updatedUser.fullName}`)
+    } catch (error) {
+        console.log(
+            "Error updating Stream user during onboarding", error.message
+        )
+    }
+
     res.status(200).json({
-        success: true,
-        message: "Logout successful"
-    })
+      success: true,
+      user: updatedUser,
+    });
+  } catch (error) {
+    console.log("Onboarding Error: ", error);
+    res.status(500).json({
+      message: "Internal Server Error",
+    });
+  }
 }
--- backend/src/server.js
+++ backend/src/server.js
@@ -1,17 +1,21 @@
 import express from "express";
 import dotenv from "dotenv";
+import cookieParser from "cookie-parser"
 
 dotenv.config();
 
 import authRoutes from "./routes/auth.route.js";
+import userRoutes from "./routes/user.route.js"
 import { connectDB } from "./lib/db.js";
 
 const app = express();
 const PORT = process.env.PORT || 5001;
 
 app.use(express.json());
+app.use(cookieParser())
 
 app.use("/api/auth", authRoutes);
+app.use("/api/users", userRoutes);
 
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
